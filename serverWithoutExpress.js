6d98bb6199bf58ef9ef3de5674be68edd6d4b3bc
soheon-lee <lsheon93@gmail.com>
2022-03-13 13:52:32 +0900

    Add: serverWithoutExpress with routing

--- serverWithoutExpress.js
+++ serverWithoutExpress.js
@@ -1,9 +1,33 @@
 const http = require('http')
+const { sendProducts } = require('./sendProducts')
+
+console.log(sendProducts)
 
 const server = http.createServer((req, res) => {
   console.log('request received')
 
+  const { url, method } = req
+
   res.setHeader('Content-Type', 'application/json')
+
+  if (url === '/ping') {
+    return res.end(JSON.stringify({ message: '/pong' }))
+  }
+
+  if (url === '/signup' && method === 'POST') {
+    return res.end(
+      JSON.stringify({ message: '회원가입 완료!' })
+    )
+  }
+
+  if (url === '/login' && method === 'POST') {
+    return res.end(
+      JSON.stringify({ message: '로그인 완료!' })
+    )
+  }
+
+  if (url === '/products' && method === 'GET') { return sendProducts(res) }
+
   res.end(JSON.stringify({ message: "Welcome to >wecode server! Http server without express" }))
 });
 
